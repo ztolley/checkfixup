eb24401883305a1bbc941ce8066e5ca67db9e853
Zac Tolley <zac@thetolleys.com>
2017-12-23 10:33:08 +0000

    Split logging and add tests
    Splits the log functions into their own files
    Adds tests to recreate undefined in slack messages
    Fix issue with undefined appearing in slack messages

---
+++ src/logging/log_event.test.js
@@ -0,0 +1,90 @@
+describe('log event', () => {
+  beforeEach(() => {
+    this.sandbox = sinon.sandbox.create()
+
+    this.axiosGetStub = this.sandbox.stub()
+
+    this.logEvent = proxyquire(`${__dirname}/log_event`, {
+      'axios': {
+        get: this.axiosGetStub
+      },
+      'uuid': this.sandbox.stub().returns('4444')
+    })
+  })
+
+  afterEach(() => {
+    this.sandbox.restore()
+  })
+
+  context('when there is a GA key', () => {
+    beforeEach(() => {
+      process.env.GA = '1234'
+    })
+
+    context('when called with a success', () => {
+      beforeEach(async () => {
+        await this.logEvent('success')
+      })
+
+      it('should log a success event with GA', () => {
+        expect(this.axiosGetStub).to.be.calledWith('https://ga-dev-tools.appspot.com/hit-builder/', {
+          params: {
+            v: 1,
+            t: 'event',
+            tid: '1234',
+            cid: '4444',
+            ec: 'checkfixup',
+            ea: 'setStatus',
+            ev: 1
+          }
+        })
+      })
+    })
+
+    context('when called with a failure', () => {
+      beforeEach(async () => {
+        await this.logEvent('failure')
+      })
+
+      it('should log a success event with GA', () => {
+        expect(this.axiosGetStub).to.be.calledWith('https://ga-dev-tools.appspot.com/hit-builder/', {
+          params: {
+            v: 1,
+            t: 'event',
+            tid: '1234',
+            cid: '4444',
+            ec: 'checkfixup',
+            ea: 'setStatus',
+            ev: 0
+          }
+        })
+      })
+    })
+  })
+
+  context('when there is no GA key', () => {
+    beforeEach(() => {
+      delete process.env.GA
+    })
+
+    context('when called with a success', () => {
+      beforeEach(async () => {
+        await this.logEvent('success')
+      })
+
+      it('should not log a success event with GA', () => {
+        expect(this.axiosGetStub).to.not.be.called
+      })
+    })
+
+    context('when called with a failure', () => {
+      beforeEach(async () => {
+        await this.logEvent('failure')
+      })
+
+      it('should not log a success event with GA', () => {
+        expect(this.axiosGetStub).to.not.be.called
+      })
+    })
+  })
+})
